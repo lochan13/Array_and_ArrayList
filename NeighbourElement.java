9e9e01991f88eba9c7a2c5ab6f7882da515aa746
lochan paudel <lochanpaudel10@gmail.com>
2025-02-03 20:51:14 +0530

    created the main function for the NeighbourElement.java

--- NeighbourElement.java
+++ NeighbourElement.java
@@ -20,28 +20,25 @@ public class NeighbourElement {
         return minIndex;
     }
 
-    // public static void main(String[] args) {
-    //     Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-    //     // Take array input
-    //     System.out.print("Enter the number of elements: ");
-    //     int n = scanner.nextInt();
+        // Take array input
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
 
-    //     if (n < 2) {
-    //         System.out.println("Array must have at least two elements.");
-    //         return;
-    //     }
+        
 
-    //     int[] arr = new int[n];
-    //     System.out.println("Enter " + n + " integers:");
-    //     for (int i = 0; i < n; i++) {
-    //         arr[i] = scanner.nextInt();
-    //     }
+        int[] arr = new int[n];
+        System.out.println("Enter " + n + " integers:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
 
-    //     // Find and print the result
-    //     int index = findSmallestNeighborIndex(arr);
-    //     System.out.println("Index of the first number: " + index);
+        // Find and print the result
+        int index = findSmallestNeighborIndex(arr);
+        System.out.println("Index of the first number: " + index);
 
-    //     scanner.close();
-    // }
+        scanner.close();
+    }
 }
